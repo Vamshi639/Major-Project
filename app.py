9a491ddc08b5945c49b6ea7ddf257d716172a4f2
Manoj-Kumar-Paliviri <83910643+Manoj-Kumar-Paliviri@users.noreply.github.com>
2021-06-05 17:40:38 +0530

    Delete app.py

--- app.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-import joblib
-model = joblib.load('Sentiment_Analyzer')
-st.title('Sentiment Analyzer')
-input = st.text_input('Enter your review:')
-output = model.predict([input])
-if st.button('Predict'):
-  st.title(output[0])
