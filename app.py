051a69da0754701ade548681f6b3ff1b907251da
Manoj-Kumar-Paliviri <83910643+Manoj-Kumar-Paliviri@users.noreply.github.com>
2021-06-05 16:13:05 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import joblib
+model = joblib.load('Sentiment_Analyzer')
+st.title('Sentiment Analyzer')
+input = st.text_input('Enter your review:')
+output = model.predict([input])
+ans = output[0]
+if st.button('Predict'):
+  st.title(output[0])
